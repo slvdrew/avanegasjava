64e6f51d247bd38626c800f5a37d268cb31128e0
Andrew Vanegas <70233300+slvdrew@users.noreply.github.com>
2020-09-12 20:23:00 -0700

    initial commit

---
+++ main.js
@@ -0,0 +1,19 @@
+var firstName = "Andrew";
+
+var lastName = "Vanegas";
+
+let myAge = 20;
+
+alert("My name is " + firstName + " " + lastName);
+
+alert("I am " + myAge + " years old ");
+
+if (myAge < 21) {
+    alert("It is illegal for me to drink in the states.");
+  } else if (myAge > 21) {
+    alert("I am legally able to drink in the states.");
+  } else {
+    alert("I am just barely able to drink legally in the states.");
+  }
+
+console.log("I am having a blast in this class!");
